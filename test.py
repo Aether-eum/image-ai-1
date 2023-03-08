f21686b086950dca6c80aebd4d45a683a1ebab35
Aether <thewaychin@gmail.com>
2023-03-07 20:33:22 -0600

    retrain nano model

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 from ultralytics import YOLO
+import numpy as np
 
 # Load a model
-model = YOLO("runs/classify/yolov8m_v8_20e/weights/best.pt")  # load a custom model
+model = YOLO("runs/classify/yolov8n_v8_10e/weights/best.pt")  # load a custom model
 
 # Predict with the model
 results = model("/home/aether/pycharm/ai1/dataset/valid/5/191.png")  # predict on an image
 
 print(results)
+
+# get pred tensor
+pred = results[0].probs
+# from tensor to numpy
+pred_arr = pred.cpu().numpy()
+# get the max value
+print(pred_arr)
+pred = np.max(pred_arr)
+print(pred)
+# find the index of the max value
+pred_index = np.where(pred_arr == pred)
+# print the index
+print(pred_index[0][0]+1, pred)
--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # Load the model.
-# model = YOLO('yolov8n-cls.pt')
+model = YOLO('yolov8n-cls.pt')
 # model = YOLO('yolov8l-cls.pt')
-model = YOLO('yolov8m-cls.pt')
+# model = YOLO('yolov8m-cls.pt')
 
 
 
@@ -12,9 +12,9 @@ results = model.train(
     data='/home/aether/pycharm/ai1/dataset',
     task='classify',
     imgsz=448,
-    epochs=20,
+    epochs=10,
     batch=4,
-    name='yolov8m_v8_20e'
+    name='yolov8n_v8_10e'
 )
 
 # yolo task=classify mode=val model=runs/classify/yolov8l_v8_50e/weights/best.pt name=yolov8l_eval data=/home/aether/pycharm/ai1/dataset imgsz=1280
